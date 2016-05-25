6808a1e4c4ac1ad0b6ae358dd0353c34c793e4f4
Chai Phonbopit <phonbopit@gmail.com>
2016-05-25 17:36:55 +0700

    check response before return

--- src/server.js
+++ src/server.js
@@ -12,17 +12,31 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+
+// TODO: match with regex
+//app.get('/service/mobile/:mobileNo')
+// match /service/mobile/12345
+// match /service/mobile/55555
+// match /service/mobile/:mobileNo
+
 app.get('*', (req, res) => {
 
   const path = req.path;
+
+  let matched = path.match(/:\w+/g);
 
   Model
   .findOne({url: path, method: 'GET'})
   .exec((err, result) => {
     if (err) return err;
 
-    const response = JSON.parse(result.response);
-    return res.json(response);
+    if (result) {
+      const response = JSON.parse(result.response);
+      return res.json(response);
+    } else {
+      return res.json({});
+    }
+
   });
 
 });
@@ -36,8 +50,12 @@ app.post('*', (req, res) => {
   .exec((err, result) => {
     if (err) return err;
 
-    const response = JSON.parse(result.response);
-    return res.json(response);
+    if (result) {
+      const response = JSON.parse(result.response);
+      return res.json(response);
+    } else {
+      return res.json({});
+    }
   });
 });
 
