1c3e30bfd5274f6c0e00344ece12fc225c0a9c06
Chai Phonbopit <phonbopit@gmail.com>
2016-05-25 16:40:35 +0700

    update server response

--- src/server.js
+++ src/server.js
@@ -3,12 +3,42 @@
 const express = require('express');
 const app = express();
 
-const { APP_PORT } = require('./config');
-
 const db = require('./db')() // invoke db.
 
-app.get('/', function (req, res) {
+const { APP_PORT } = require('./config');
+const Model = require('./models/endpoint.model.js');
+
+app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('*', (req, res) => {
+
+  const path = req.path;
+
+  Model
+  .findOne({url: path, method: 'GET'})
+  .exec((err, result) => {
+    if (err) return err;
+
+    const response = JSON.parse(result.response);
+    return res.json(response);
+  });
+
+});
+
+app.post('*', (req, res) => {
+
+  const path = req.path;
+
+  Model
+  .findOne({url: path, method: 'POST'})
+  .exec((err, result) => {
+    if (err) return err;
+
+    const response = JSON.parse(result.response);
+    return res.json(response);
+  });
 });
 
 app.listen(APP_PORT, function () {
